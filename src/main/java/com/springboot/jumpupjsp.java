ce49b168754b9dd282f7658450bae412c5b46831
Gruuy <245746119@qq.com>
2019-03-11 12:59:17 +0800

    Initial commit

---
+++ src/main/java/com/springboot/jumpupjsp.java
@@ -0,0 +1,12 @@
+package com.springboot;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class jumpupjsp {
+    @RequestMapping(value = "/success")
+    public String success(){
+        return "success";
+    }
+}
